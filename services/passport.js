b64315692d61dbae37c0f9c50949fe754794dd21
rickyjw <rickywid@hotmail.com>
2019-07-08 18:17:07 -0400

    create a new Bookshelf record first and then create a new user record and use the id of the new Bookshelf record as the user's list_id

--- services/passport.js
+++ services/passport.js
@@ -29,9 +29,14 @@ const googleStrategy = new GoogleStrategy({
       values: [profile.emails[0].value],
     }
 
+
+    // create a new Bookshelf record first and then create a new user record and use the id of the new Bookshelf record as the user's list_id
     const query2 = {
-      text: 'INSERT INTO Users(email) VALUES($1)',
-      values: [profile.emails[0].value],
+      text: `with t1 AS(INSERT INTO Bookshelf(backlog, currently, completed, like_count, created_at) VALUES('[]','[]','[]',0,NOW()) RETURNING id) 
+                  INSERT INTO Users (name, email, list_id, created_at)
+
+              SELECT $1, $2, t1.id, NOW() FROM t1 RETURNING id`,
+      values: [profile.displayName, profile.emails[0].value],
     }
     
     const q1 = await pool.query(query1);
@@ -39,6 +44,8 @@ const googleStrategy = new GoogleStrategy({
     if(q1.rows.length === 0) {
       // user doesnt exist, add user to db
       const q2 = await pool.query(query2);
+
+
       console.log('not found');
       return cb(null, q2);  
       
